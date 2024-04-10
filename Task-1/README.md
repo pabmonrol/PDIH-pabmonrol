de06aa5aac0f2fe3224ad9342de8b174ae3ae5e1
Pablo Montero Rollán <pabmonrol@alum.us.es>
2024-04-10 03:34:51 +0200

    Task 1

---
+++ Task-1/README.md
@@ -0,0 +1,42 @@
+# Task 1: Entrada/Salida utilizando interrupciones con lenguaje C
+
+## Description
+El objetivo de la práctica es aprender programación de interrupciones de software, entender la interfaz de acceso a recursos de entrada/salida, y desarrollar una biblioteca de funciones básicas de E/S.
+
+## Exercises
+
+### Requisitos mínimos
+
+#### 6.1 *gotoxy(int x, int y)*: Place the cursor at a specific position
+Positions the cursor at the specified coordinates (x, y) on the screen. This function is useful for controlling where text or characters are displayed on the screen.
+
+#### 6.2 *setcursortype(int cursor_type)*: To set the appearance of the cursor, it must support three values: INVISIBLE, NORMAL and THICK.
+Changes the cursor's appearance based on the given cursor_type, which can be INVISIBLE, NORMAL, or THICK. This allows for customization of the cursor's visibility and shape according to the user's preference or the application's requirements.
+
+#### 6.3 *setvideomode(BYTE mode)*: Set the desired video mode
+Sets the video mode to the specified mode. This function enables switching between different video modes (e.g., text mode, graphics mode) supported by the DOS system.
+
+#### 6.4 *getvideomode()*: Gets the current video mode
+Returns the current video mode. This function is useful for retrieving the current state of the video mode, which can be helpful for conditional logic based on the video mode or for restoring a previous video mode.
+
+#### 6.5 *textcolor()*: Modifies the foreground color with which the characters will be displayed
+Prompts the user to enter a color number for the text and sets the text color to that value. This function enhances the visual aspect of text output by allowing different colors.
+
+#### 6.6 *textbackground()*:Modifies the background color with which the characters will be displayed
+This function prompts the user to enter a color number but sets the background color of the text to that value. It provides a way to customize the background color of text for better readability or aesthetic purposes.
+
+#### 6.7 *clrscr()*: Clear the entire screen
+Clears the screen by printing a series of newline characters. This simple approach to clearing the screen is used for creating a fresh view or removing previous outputs from the screen.
+
+#### 6.8 *cputchar(char c)*: Writes a character on the screen with the currently indicated color
+Writes a single character c to the screen using the currently set text and background colors. This function is essential for displaying characters with specific color attributes.
+
+#### 6.9 *getche()*: Gets a keyboard character and displays it on the screen
+Gets a character from the keyboard and displays it immediately on the screen. It combines input reading and output display in a single step, useful for interactive applications.
+
+
+### Requisitos ampliados
+
+#### 7.1
+
+#### 7.2
---
+++ Task-1/RequisitosMinimos.C
@@ -0,0 +1,194 @@
+#include <dos.h>
+#include <stdio.h>
+
+#define INVISIBLE 0
+#define NORMAL 1
+#define THICK 2
+
+#define BYTE unsigned char
+
+BYTE SMALL=3;
+BYTE BIG=4;
+
+// Global variables created for the correct operation of the functions related to exercises 4, 5, and 6
+unsigned char textColor = 7;
+unsigned char textBackground = 0;
+unsigned char currentVideoMode;
+
+// Helper function to wait for user interaction. If the ESC key is pressed, the program exits.
+void waitForKey() {
+    union REGS regs;
+    regs.h.ah = 0x00;
+    int86(0x16, &regs, &regs);
+	
+	if (regs.h.al == 0x1B) { 
+        printf("\nESC key pressed. Exiting the program...\n");
+        exit(0);
+    }
+}
+
+// Exercise 1: Places the cursor at a specified position
+void gotoxy(int x, int y) {
+    union REGS regs;
+    regs.h.ah = 0x02;
+    regs.h.dh = y;
+    regs.h.dl = x;
+    regs.h.bh = 0x00;
+    int86(0x10, &regs, &regs);
+}
+
+// Exercise 2: Sets the cursor appearance, it must accept three values: INVISIBLE, NORMAL, and THICK.
+void setcursortype(int cursor_type) {
+    union REGS regs;
+    regs.h.ah = 0x01;
+    switch(cursor_type) {
+        case INVISIBLE:
+            regs.h.ch = 0x20;
+						break;
+        case NORMAL:
+            regs.h.ch = 0x06;
+            regs.h.cl = 0x07;
+            break;
+        case THICK:
+            regs.h.ch = 0x00;
+            regs.h.cl = 0x0B;
+            break;
+    }
+    int86(0x10, &regs, &regs);
+}
+
+// Exercise 3: Sets the desired video mode
+void setvideomode(BYTE mode) {
+    union REGS inregs, outregs;
+	 inregs.h.ah = 0;
+	 inregs.h.al = mode;
+	 int86(0x10,&inregs,&outregs);
+	 return;
+}
+
+// Exercise 4: Gets the current video mode
+unsigned char getvideomode() {
+    int mode;
+    union REGS inregs, outregs;
+    inregs.h.ah = 15;
+    int86(0x10, &inregs, &outregs);
+    mode = outregs.h.al;
+    return mode;
+}
+
+// Exercise 5: Changes the foreground color of the characters to be displayed
+// It is expected that the user enters the numeric value of the color they want to change to.
+void textcolor() {
+    unsigned char color;
+    printf("\nEnter the color number for the text and press Enter: ");
+    scanf("%hhu", &color);
+    textColor = color;
+}
+
+// Exercise 6: Changes the background color of the characters to be displayed
+// It is expected that the user enters the numeric value of the color they want to change to.
+void textbackground() {
+    unsigned char color;
+    printf("\nEnter the color number for the background and press Enter: ");
+    scanf("%hhu", &color);
+    textBackground = color;
+}
+
+// Exercise 7: Clears the entire screen
+void clrscr() {
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+}
+
+// Exercise 8: Writes a character on the screen with the currently indicated color
+void cputchar(char c) {
+    union REGS regs;
+    regs.h.ah = 0x09;
+    regs.h.al = c;
+    regs.h.bl = (textBackground << 0x04) | textColor;
+    regs.h.bh = 0x00;
+    regs.x.cx = 0x01;
+    int86(0x10, &regs, &regs);
+}
+
+// Exercise 9: Gets a character from the keyboard and displays it on the screen
+int getche() {
+    union REGS regs;
+    regs.h.ah = 0x00;
+    int86(0x16, &regs, &regs);
+    printf("%c", regs.h.al); // Displays the read character
+    return regs.h.al;
+}
+
+int main() { 
+	printf("\nYou can exit the program by pressing the ESC key. Press any key to continue...");
+	waitForKey();
+	
+	// Exercise 7: 
+	printf("\nBy pressing a key you will execute the function of exercise 7. Press another key to execute the next exercise.");
+	waitForKey();
+	clrscr();
+	waitForKey();
+	
+	// Exercise 8
+    printf("\nWriting a character 'A' at the current position | Exercise 8. Press a key to continue...");
+	cputchar('A');
+    waitForKey();
+	
+	// Exercise 1
+	gotoxy(10, 10);
+	cputchar('X');
+    printf("\nMoving the cursor to position (10, 10) | Exercise 1. Press a key to continue...");
+    waitForKey();
+	
+	// Exercise 9
+    printf("\nReading a character from the keyboard and displaying it on the screen | Exercise 9. Press the letter you want to display...");
+    getche();
+    
+	// Exercise 5
+	printf("\nChanging text color | Exercise 5.");
+	textcolor();
+	cputchar('T');
+
+	// Exercise 6
+    printf("\nChanging background color | Exercise 6.");
+	textbackground();
+    cputchar('F');
+	
+	// Exercise 3.1
+	printf("\nChanging the cursor type | Exercise 3. Press a key to continue...");
+	waitForKey();
+	setcursortype(INVISIBLE);
+	printf("\nChanging the cursor type to INVISIBLE. Press a key to continue...");
+    waitForKey();
+	
+	// Exercise 3.2
+    setcursortype(THICK);
+	printf("\nChanging the cursor type to THICK. Press a key to continue...");
+    waitForKey();
+	
+	// Exercise 3.3
+    setcursortype(NORMAL);
+	printf("\nChanging the cursor type to NORMAL. Press a key to continue...");
+    waitForKey();
+	
+	// Exercise 4
+    printf("\nSetting the video mode to text | Exercise 4. Press a key to continue...");
+    waitForKey();
+    setvideomode(BIG);
+    printf("\nThe video mode has been changed");
+	waitForKey();
+	
+	// Exercise 2
+	printf("\nGetting the new video mode | Exercise 2. Press a key to continue...");
+    waitForKey();
+    currentVideoMode = getvideomode();
+    printf("\nThe new video mode is: 0x%X", currentVideoMode);
+	setvideomode(SMALL);
+	
+	// Exercise 7
+	printf("\nClearing the screen. Press a key to continue...");
+    waitForKey();
+    clrscr();
+
+    return 0;
+}
